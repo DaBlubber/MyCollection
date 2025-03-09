4bac8a0cbbd6b7a98ef4ea266b6caa06bc0b5833
freuland <florian@nerdyadmins.de>
2025-03-09 14:16:34 +0100

    Update BackupJobs/bash/backupmariadb.sh

---
+++ BackupJobs/bash/backupmariadb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Name des Docker Containers
+CONTAINER_NAME="mariadb"
+
+# Pfad zum Backup Verzeichnis
+BACKUP_DIR="/usr/local/container/backup/MariaDB"
+
+# Name der Datenbank
+DATABASENAME=("DB1" "DB2")
+
+# Username und Passwort
+USERNAME="USER"
+PASSWORD="PASSWORT"
+
+# Datum im Format JJJJMMTT
+DATE=$(date +%Y%m%d)
+
+# Ausführen des Backup Commands
+for DB in "${DATABASENAME[@]}"; do 
+docker exec -e MYSQL_PWD=${PASSWORD} ${CONTAINER_NAME} /usr/bin/mariadb-dump -u ${USERNAME} ${DB} > ${BACKUP_DIR}/mariadbbak_${DB}_${DATE}.sql
+zip ${BACKUP_DIR}/mariadbbak_${DB}_${DATE}.zip ${BACKUP_DIR}/mariadbbak_${DB}_${DATE}.sql
+rm ${BACKUP_DIR}/mariadbbak_${DB}_${DATE}.sql
+done
+
+echo "Backup Completed!"
